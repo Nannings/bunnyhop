63f3b3200a2b72b14b7dd12d0d9a5234d8dd2aba
Nannings <mrnannings@gmail.com>
2019-02-05 13:33:19 +0100

    5

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,25 @@
 extends KinematicBody2D
 
 const SPEED = 750
+const GRAVITY = 3600
+const UP = Vector2(0, -1)
+const JUMP_SPEED = -1750
 
 var motion = Vector2()
 
 func _physics_process(delta):
+	run()
+	fall(delta)
+	jump()
+	move_and_slide(motion, UP)
+
+func fall(delta):
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY * delta
+
+func run():
 	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
 		$AnimatedSprite.play("run")
@@ -17,4 +32,20 @@ func _physics_process(delta):
 		motion.x = 0
 		$AnimatedSprite.play("idle")
 		$AnimatedSprite.flip_h = false
-	move_and_slide(motion)
+
+func jump():
+	if is_on_floor() && Input.is_action_pressed("ui_up"):
+		motion.y = JUMP_SPEED
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
